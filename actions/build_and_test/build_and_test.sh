2645c1585f2c55480da3b8f5c354223733b9bc76
quazirafi <quaziishtiaquemahmud@Quazis-MacBook-Pro.local>
2022-11-18 22:18:55 -0600

    ignoring coverage report

--- actions/build_and_test/build_and_test.sh
+++ actions/build_and_test/build_and_test.sh
@@ -81,9 +81,9 @@ ${ctest_command} -VV
 cd ..
 
 #Step 5: Generate coverage report
-curr_dir=$(pwd)
-cd ..
-gcovr --root "${curr_dir}" \
-      --filter "${curr_dir}" \
-      --exclude "${curr_dir}"/tests \
-      --xml "${curr_dir}"/coverage.xml
+# curr_dir=$(pwd)
+# cd ..
+# gcovr --root "${curr_dir}" \
+#       --filter "${curr_dir}" \
+#       --exclude "${curr_dir}"/tests \
+#       --xml "${curr_dir}"/coverage.xml
