5a703c1d2af69dd26fe2fbfd730de771d5888116
quazirafi <quaziishtiaquemahmud@Quazis-MacBook-Pro.local>
2023-01-31 23:14:13 -0600

    create html in build

--- actions/build_docs/build_docs.sh
+++ actions/build_docs/build_docs.sh
@@ -39,11 +39,9 @@ ${cmake_command} --build build --target "${doc_target}"
 # Step 2: Migrate the Doxygen documentation to the docs source
 # mkdir docs/build
 # mkdir docs/build/html
-ls
-pwd
 cd build
-echo "inside build"
-ls
+mkdir html
+cd ..
 mv build/html "docs/build/html/${doc_target}"
 echo "this is the file location"
 echo "docs/build/html/${doc_target}"
