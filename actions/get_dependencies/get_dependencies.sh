4ebc1868b5a82b382a50feb528f1780b5273d01f
Hung PHAN <pdhung3012@gmail.com>
2023-02-08 14:06:47 -0600

    Update get_dependencies.sh

--- actions/get_dependencies/get_dependencies.sh
+++ actions/get_dependencies/get_dependencies.sh
@@ -172,20 +172,20 @@ get_libint() {
     cd libint-2.6.0
     export CXX=`which g++`
     export CC=`which gcc`
-#     ../cmake-3.16.3-Linux-x86_64/bin/cmake -H. -Bbuild -DCMAKE_INSTALL_PREFIX=${INSTALL_PATH} -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_FLAGS="-std=c++17" -DBUILD_SHARED_LIBS=ON -DCPP_GITHUB_TOKEN=$CPP_GITHUB_TOKEN
-#     cd build
-    mkdir -p build/abc/def/
+    ../cmake-3.16.3-Linux-x86_64/bin/cmake -H. -Bbuild -DCMAKE_INSTALL_PREFIX=${INSTALL_PATH} -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_FLAGS="-std=c++17" -DBUILD_SHARED_LIBS=ON -DCPP_GITHUB_TOKEN=$CPP_GITHUB_TOKEN
+    cd build
 #   pwd
 #   echo "above is the file path"
+    make
+    make install
+    mkdir -p build/abc/def/
     cd build/abc/def/
     echo "hello" >> hello.txt
-#     make
-#     make install
   else
     cd libint-2.6.0/
     ls build/abc/def/
     echo "relax"
-    ls
+#     ls
   fi
 }
 # Wraps installing ninja
