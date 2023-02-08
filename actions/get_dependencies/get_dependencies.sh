486d3b7d88bea46574f4cac284f5572904147819
Hung PHAN <pdhung3012@gmail.com>
2023-02-08 12:49:46 -0600

    Update get_dependencies.sh

--- actions/get_dependencies/get_dependencies.sh
+++ actions/get_dependencies/get_dependencies.sh
@@ -163,8 +163,8 @@ get_lapacke() {
 get_libint() {
   echo "hehehe CACHE_LIBINT " "${CACHE_LIBINT}" "abc"
 #   env
-  if ! $CACHE_LIBINT ; then
-    echo "go inside " "${CACHE_LIBINT}" "abc"
+  if [[ ! $CACHE_LIBINT ]] then
+    echo "go inside " " $CACHE_LIBINT " "abc"
     export INSTALL_PATH=`pwd`/install
 #     # check code out
     wget https://github.com/evaleev/libint/releases/download/v2.6.0/libint-2.6.0.tgz
@@ -173,7 +173,7 @@ get_libint() {
     export CXX=`which g++`
     export CC=`which gcc`
     ../cmake-3.16.3-Linux-x86_64/bin/cmake -H. -Bbuild -DCMAKE_INSTALL_PREFIX=${INSTALL_PATH} -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_FLAGS="-std=c++17" -DBUILD_SHARED_LIBS=ON -DCPP_GITHUB_TOKEN=$CPP_GITHUB_TOKEN
-  cd build
+    cd build
 #   cd ../
 #   mkdir build
 #   pwd
