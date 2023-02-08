fef3682f685d3a33f6acf9e56090b91870ff77ca
Hung PHAN <pdhung3012@gmail.com>
2023-02-08 17:02:22 -0600

    Update build_and_test.sh

--- actions/build_and_test/build_and_test.sh
+++ actions/build_and_test/build_and_test.sh
@@ -25,7 +25,7 @@ cmake_root=$(pwd)/cmake-"${cmake_version}"-"${arch}"
 cmake_command=cmake #"${cmake_root}/bin/cmake"
 ctest_command=ctest #"${cmake_root}/bin/ctest"
 toolchain_file=$(pwd)/toolchain.cmake
-export Libint2_DIR=$(pwd)/libint-2.6.0/build/
+export Libint2_DIR=$(pwd)/libint-2.6.0/
 echo "relect on build and test " ${Libint2_DIR}
 
 echo "set(BUILD_TESTING ON)" > "${toolchain_file}"
