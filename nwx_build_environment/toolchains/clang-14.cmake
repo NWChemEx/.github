69f23212e99133ecbc6d3b62f75e6448289fb334
Jonathan M. Waldrop <relik107@gmail.com>
2024-05-02 16:07:38 -0500

    update clang toolchain to use libc++

--- nwx_build_environment/toolchains/clang-14.cmake
+++ nwx_build_environment/toolchains/clang-14.cmake
@@ -1,2 +1,5 @@
 set(CMAKE_C_COMPILER clang-14)
 set(CMAKE_CXX_COMPILER clang++-14)
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++ -lc++abi")
