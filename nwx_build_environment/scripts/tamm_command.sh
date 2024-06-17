b4acc809bb76622d77ccbeaf2e99eb200339fa73
Jonathan M. Waldrop <relik107@gmail.com>
2024-06-17 16:08:23 -0500

    update tamm_command

--- nwx_build_environment/scripts/tamm_command.sh
+++ nwx_build_environment/scripts/tamm_command.sh
@@ -1,2 +1,4 @@
 CC=gcc CXX=g++ FC=gfortran cmake -Bbuild -H. -DCMAKE_INSTALL_PREFIX=/nwx_dependencies/gcc-11 -DCMAKE_BUILD_TYPE=Release -DBLAS_INT4=ON -DMODULES="CC;DFT"
-cmake --build build --target install
+cd build
+make -j2
+make install
