64e90c3f3219d48a80febf8e85e62f35a01272f2
Ajay Panyala <ajay.panyala@gmail.com>
2024-07-15 12:03:07 -0700

    Update tamm_command.sh (#156)

--- nwx_build_environment/scripts/tamm_command.sh
+++ nwx_build_environment/scripts/tamm_command.sh
@@ -1,4 +1,4 @@
-CC=gcc CXX=g++ FC=gfortran cmake -Bbuild -H. -DCMAKE_INSTALL_PREFIX=/nwx_dependencies/gcc-11 -DCMAKE_BUILD_TYPE=Release -DBLAS_INT4=ON -DMODULES="CC;DFT" -DCMAKE_TOOLCHAIN_FILE=/toolchains/tamm.cmake -DUSE_LIBNUMA=OFF
+CC=gcc CXX=g++ FC=gfortran cmake -Bbuild -H. -DCMAKE_INSTALL_PREFIX=/nwx_dependencies/gcc-11 -DCMAKE_BUILD_TYPE=Release -DBLAS_INT4=ON -DMODULES="DFT" -DCMAKE_TOOLCHAIN_FILE=/toolchains/tamm.cmake -DUSE_LIBNUMA=OFF
 cd build
 make -j2
 make install
