8a60eed683fc59486ec4c0b9ad02edc419e79e32
Jonathan M. Waldrop <relik107@gmail.com>
2025-12-11 13:10:11 -0600

    Remove stale standard specification (#183)
    * Remove stale standard specification
    
    * remove extra newline

--- nwx_build_environment/toolchains/nwx_base.cmake
+++ nwx_build_environment/toolchains/nwx_base.cmake
@@ -20,7 +20,7 @@ set(CMAKE_BUILD_TYPE Debug)
 
 list(APPEND CMAKE_PREFIX_PATH "/nwx_dependencies/${CMAKE_C_COMPILER}")
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage -std=c++17 -Wall")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage -Wall")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DOMPI_SKIP_MPICXX")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --coverage")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fprofile-arcs")
